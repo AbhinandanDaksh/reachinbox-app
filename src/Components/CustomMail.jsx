baaa582a752c8c69792baf1bd481baac63d8a838
Abhinandan Daksh <Abhinandandaksh@gmail.com>
2024-08-13 21:45:48 +0530

    CustomMail

--- src/Components/CustomMail.jsx
+++ src/Components/CustomMail.jsx
@@ -115,7 +115,7 @@ function CustomMail({ threadId, onClose }) {
           />
         </div>
 
-        <div className="flex space-x-8 items-center h-20 ml-8">
+        <div className="flex space-x-8 items-center h-12 ml-8">
           <div
             className="bg-gradient-to-r from-[#4B63DD] to-[#0524BFFC] px-5 py-2 rounded-md flex items-center cursor-pointer"
             onClick={handleSendReply}
